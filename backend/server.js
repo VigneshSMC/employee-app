045791a9cd33891f64b459e2d4add67b0939237d
Ubuntu <ubuntu@ip-172-31-38-111.us-east-2.compute.internal>
2026-06-18 09:12:09 +0000

    CI/CD changes

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,14 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
+const mongoose = require("mongoose")
+const app = require("./app")
 
 mongoose.connect(
 "mongodb://mongodb:27017/employeedb"
 )
-.then(()=>console.log("MongoDB Connected"))
-.catch(err=>console.log(err));
-
-const EmployeeSchema = new mongoose.Schema({
-   name:String,
-   department:String
-});
-
-const Employee =
-mongoose.model("Employee",EmployeeSchema);
-
-app.get("/employees",async(req,res)=>{
-   const employees =
-   await Employee.find();
-
-   res.json(employees);
-});
-
-app.post("/employees",async(req,res)=>{
-
-   const employee =
-   new Employee(req.body);
-
-   await employee.save();
-
-   res.json(employee);
-});
-
-app.listen(5000,()=>{
-   console.log("Server Running");
-});
+.then(()=>{
+	console.log("MongoDB Connected")
+	app.listen(5000, () => {
+		console.log("server started")
+	})
+})
+.catch(err=>console.log(err))
 
